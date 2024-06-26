ec040839fbb24da96f4f1833aefebe168f8bc0f3
manmohan1manu <manmohan.zinreet@gmail.com>
2024-06-26 17:49:10 +0530

    trigger lambda function on S3 post event

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+const s3Client = new S3Client();
+
 exports.handler = async (event) => {
-    console.log(event);
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const bucketName = event.Records[0].s3.bucket.name;
+    const objectKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const fileUrl = `https://${bucketName}.s3.amazonaws.com/${objectKey}`;
+    console.log('File URL:', fileUrl);
 
     const SENDER = "manmohan.zinreet@gmail.com";
     const RECIPIENT = "manmohan.zinreet@gmail.com";
@@ -12,7 +20,8 @@ exports.handler = async (event) => {
         <head></head>
         <body>
           <h1>AWS Lambda Email Test</h1>
-          <p>This email was sent with AWS Lambda using Mailtrap SMTP 120112</p>
+          <p>This email was sent with AWS Lambda using Mailtrap SMTP 1201</p>
+          <p>File URL: <a href="${fileUrl}">${fileUrl}</a></p>
         </body>
         </html>
     `;
@@ -38,7 +47,7 @@ exports.handler = async (event) => {
         console.log(`Email sent! Message ID: ${info.messageId}`);
         return {
             statusCode: 200,
-            body: `Email sent! Message ID: ${info.messageId}`
+            body: JSON.stringify({ message: 'Email sent successfully!', messageId: info.messageId, fileUrl: fileUrl })
         };
     } catch (err) {
         console.error(err, err.stack);
